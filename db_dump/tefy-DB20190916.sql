2024ffe195c235f5ae235833819ffe00c7495650
sandeep <sandeepp583@gmail.com>
2019-09-16 17:55:56 +0530

    DB Updated

---
+++ db_dump/tefy-DB20190916.sql
@@ -0,0 +1,235 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 16, 2019 at 02:23 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.2.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tefy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` mediumint(8) UNSIGNED NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `description` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `description`) VALUES
+(1, 'admin', 'Administrator'),
+(2, 'members', 'General User');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `listings`
+--
+
+CREATE TABLE `listings` (
+  `id` int(11) NOT NULL,
+  `school_title` varchar(60) NOT NULL,
+  `category` text NOT NULL,
+  `keywords` longtext NOT NULL,
+  `latitude` decimal(10,0) NOT NULL,
+  `longitude` decimal(10,0) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `zipcode` varchar(6) NOT NULL,
+  `address` text NOT NULL,
+  `description` text NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `website` varchar(100) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `facebook` varchar(100) NOT NULL,
+  `twitter` varchar(100) NOT NULL,
+  `google_plus` varchar(100) NOT NULL,
+  `amenities` longtext NOT NULL,
+  `opening_hours` longtext NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `modified_at` datetime DEFAULT NULL,
+  `row_status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '0=Deleted, 1=Active, 2=Inactive'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `listings`
+--
+
+INSERT INTO `listings` (`id`, `school_title`, `category`, `keywords`, `latitude`, `longitude`, `city`, `state`, `zipcode`, `address`, `description`, `phone`, `website`, `email`, `facebook`, `twitter`, `google_plus`, `amenities`, `opening_hours`, `created_at`, `modified_at`, `row_status`) VALUES
+(1, 'Nalanda High School', 'High School', 'Nalanda,Nalanda High School,High School', '0', '0', 'Hyderabad', 'Telangana', '500038', 'Sr Nager', 'Nalanda is educational group predominantly based in South Indian states of Andhra Pradesh and Karnataka. Established in 1988 by the four founding fathers of Nalanda group, it has 7 Units under which it offers academic and extension programs. Nalanda educational institutes promote the concept of a holistic approach towards education and production of globally competent and locally efficient individuals.\r\n\r\nWhen the conventional teaching methodology became ineffective in meeting today’s requirements, there came up a need for a new approach to education. It should satiate the needs in all domains like finding the skills within them, effective communication, stress-free learning from the orthodox rote learning where the focus lies merely in the academic excellence rather than the well all-rounded development of the child, and social responsibility. The forethought was made already and a squad of innovators founded NALANDA VIDYA NIKETHAN, the ground for evolution with a touch of Learner-centric teaching methodology called Self learning.', '040-514669555', 'https://nalanda.edu.in', 'nalanda@nalanda.edu.in', 'http://www.fb.com', 'http://www.twiter.com', 'http://www.googleplus.com', '[\"Elevator in building\",\"Friendly Environment\",\"Play Ground\",\"Indoor Space for Games\",\"Free parking on premises\",\"A\\/C Classes\",\"Buses for All Routes\",\"Events\"]', '{\"1\":{\"m_opening\":\"10 AM\",\"m_closing\":\"5 PM\"},\"2\":{\"t_opening\":\"10 AM\",\"t_closing\":\"5 PM\"},\"3\":{\"w_opening\":\"10 AM\",\"w_closing\":\"5 PM\"},\"4\":{\"th_opening\":\"10 AM\",\"th_closing\":\"5 PM\"},\"5\":{\"f_opening\":\"10 AM\",\"f_closing\":\"5 PM\"},\"6\":{\"sa_opening\":\"10 AM\",\"sa_closing\":\"5 PM\"},\"7\":{\"s_opening\":\"Closed\",\"s_closing\":\"Closed\"}}', '2019-09-16 17:40:25', NULL, 1),
+(2, 'Narayana', 'Pre School & High School', 'Narayana,Narayana High School,High School', '0', '0', 'Hyderabad', 'Telangana', '500038', 'Ameerpet', 'With 40 years of Academic Excellency….. The Narayana Group is Asia’s largest educational conglomerate with over 360,000 students and 35,000 experienced teaching and non-teaching faculty in over 560 centres. Spread across 13 states, Narayana is hosting a bouquet of schools, junior colleges, engineering, medical and management institutions, coaching centres along with IAS training academy, has already set a benchmark in academic excellence by continuously delivering top and matchless results in Intra and International competitive examinations.', '040-55236155', 'https://www.narayanaschools.in/', 'info@narayanaschools.in', 'http://fb.com', 'http://twiter.com', 'http://googleplus.com', '[\"Elevator in building\",\"Friendly Environment\",\"Play Ground\",\"Indoor Space for Games\",\"Free parking on premises\",\"A\\/C Classes\",\"Buses for All Routes\",\"Events\"]', '{\"1\":{\"m_opening\":\"9 AM\",\"m_closing\":\"4 PM\"},\"2\":{\"t_opening\":\"9 AM\",\"t_closing\":\"4 PM\"},\"3\":{\"w_opening\":\"9 AM\",\"w_closing\":\"4 PM\"},\"4\":{\"th_opening\":\"9 AM\",\"th_closing\":\"4 PM\"},\"5\":{\"f_opening\":\"9 AM\",\"f_closing\":\"4 PM\"},\"6\":{\"sa_opening\":\"9 AM\",\"sa_closing\":\"4 PM\"},\"7\":{\"s_opening\":\"Closed\",\"s_closing\":\"Closed\"}}', '2019-09-16 17:43:58', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_attempts`
+--
+
+CREATE TABLE `login_attempts` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `ip_address` varchar(15) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `time` int(11) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `activation_code` varchar(40) DEFAULT NULL,
+  `forgotten_password_code` varchar(40) DEFAULT NULL,
+  `forgotten_password_time` int(11) UNSIGNED DEFAULT NULL,
+  `remember_code` varchar(40) DEFAULT NULL,
+  `created_on` int(11) UNSIGNED NOT NULL,
+  `last_login` int(11) UNSIGNED DEFAULT NULL,
+  `active` tinyint(1) UNSIGNED DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `company` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
+(1, '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', '', NULL, NULL, NULL, 1268889823, 1568622191, 1, 'Admin', 'istrator', 'ADMIN', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_groups`
+--
+
+CREATE TABLE `users_groups` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `group_id` mediumint(8) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users_groups`
+--
+
+INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
+(1, 1, 1),
+(2, 1, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `listings`
+--
+ALTER TABLE `listings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
+  ADD KEY `fk_users_groups_users1_idx` (`user_id`),
+  ADD KEY `fk_users_groups_groups1_idx` (`group_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `listings`
+--
+ALTER TABLE `listings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users_groups`
+--
+ALTER TABLE `users_groups`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
