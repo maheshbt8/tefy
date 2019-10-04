fc010b26824c818015802cad2897333384f1c060
sandeep <sandeepp583@gmail.com>
2019-10-04 12:15:53 +0530

    changes

--- application/models/Common_Model.php
+++ application/models/Common_Model.php
@@ -159,7 +159,34 @@ class Common_model extends CI_Model{
     $result['timings']=$loop;
         return $result;
     }
-    
+    function time_elapsed_string($datetime, $full = false) {
+    $now = new DateTime;
+    $ago = new DateTime($datetime);
+    $diff = $now->diff($ago);
+
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+
+    $string = array(
+        'y' => 'year',
+        'm' => 'month',
+        'w' => 'week',
+        'd' => 'day',
+        'h' => 'hour',
+        'i' => 'minute',
+        's' => 'second',
+    );
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
     /*******rating of a product*******/
     function rating_of_product( $table, $where = '', $type_col = ''){
 $rating=0;
