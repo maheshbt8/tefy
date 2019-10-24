b2fd1d6cc6bd474a95459695bc263578861677c5
sandeep <sandeepp583@gmail.com>
2019-10-24 19:54:00 +0530

    terms and conditions page

---
+++ application/modules/Home/views/terms.php
@@ -0,0 +1,22 @@
+<div class="container">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div id="add-listing" class="separated-form">
+                    <!-- Section -->
+                    <div class="add-listing-section margin-top-45 switcher-on">
+                        <?=$condition;?>
+                    </div>
+                    <!-- Section / End -->
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+
+
