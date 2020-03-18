235a4f97ef703edc077e3049e3bf46797e88e553
Mahesh <mahesh.bt@grepthorsoftware.com>
2020-03-18 20:35:56 +0530

    changes

--- application/views/login.php
+++
@@ -1,4 +0,0 @@
-<h1>Login with google PLus</h1>
-<a href="<?php echo $loginURL;?>">
-	Login With googlePlus
-</a>
--- application/views/template/student/topcss.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="<?php echo base_url('assets')?>/css/style.css">
-<link rel="stylesheet" href="<?php echo base_url('assets')?>/css/main-color.css" id="colors">
-<link rel="stylesheet" href="<?php echo base_url('assets/scripts')?>/jquery-selectric/selectric.css">
